361897e45e2f5d1b60aa81030854d1f10faccf15
bkrishna1114 <bkrishna1114@gmail.com>
2023-03-29 22:32:05 +0530

    updated

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'user_Resgistration.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'User_Registration.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
