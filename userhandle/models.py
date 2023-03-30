e95e3d35c4b12c1dfdb5050f7433dfbf1a9ce22c
bkrishna1114 <bkrishna1114@gmail.com>
2023-03-30 21:14:56 +0530

    models updated

--- userhandle/models.py
+++ userhandle/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+from django.contrib.auth.base_user import  AbstractBaseUser
+
+class uwser(AbstractBaseUser):
+    first_name = models.CharField(max_length=100,blank=False)
+    last_name = models.CharField(max_length=100,blank=True)
+    email = models.CharField(max_length=100,unique=True,blank=False)
+    phone_number = models.CharField(max_length=20,blank=False)
+    address = models.CharField(max_length=200,blank=False)
+    pincode = models.CharField(max_length=6,blank=False)
+    company_name = models.CharField(max_length=50,blank=False)
+
+    user_name = 'email'
+    required_fields = []
+
