7c61b345283a0212645e5f224386de3379994873
bkrishna1114 <bkrishna1114@gmail.com>
2023-03-31 11:53:52 +0530

    code updated

--- userhandle/serializers.py
+++ userhandle/serializers.py
@@ -1,16 +1,8 @@
 from . models import user
 from rest_framework import serializers
-
+from django.db import models
 
 class userserializer(serializers.ModelSerializer):
-    # first_name = models.CharField(max_length=100, blank=False)
-    # last_name = models.CharField(max_length=100, blank=True)
-    # email = models.CharField(max_length=100, unique=True, blank=False)
-    # phone_number = models.CharField(max_length=20, blank=False)
-    # address = models.CharField(max_length=200, blank=False)
-    # pincode = models.CharField(max_length=6, blank=False)
-    # company_name = models.CharField(max_length=50, blank=False)
-
     class Meta:
         model = user
         fields='__all__'
@@ -25,4 +17,4 @@ class userserializer(serializers.ModelSerializer):
         if password is not None:
             instance.set_password((password))
         instance.save()
-        return  instance
+        return instance
